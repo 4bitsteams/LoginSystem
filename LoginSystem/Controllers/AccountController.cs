c981bf9f615698bc7372b0f8296910f7ddfacc0a
rubel islam <rubelislam301@gmail.com>
2021-03-24 11:08:28 +0600

    Implement SigninManager and User Manager By Default Identity  #4

--- LoginSystem/Controllers/AccountController.cs
+++ LoginSystem/Controllers/AccountController.cs
@@ -2,12 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LoginSystem.Controllers
 {
     public class AccountController : Controller
     {
-       
+        private readonly SignInManager<IdentityUser> signInManager;
+        private readonly UserManager<IdentityUser> userManager;
+
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            this.signInManager = signInManager;
+            this.userManager = userManager;
+        }
     }
 }
